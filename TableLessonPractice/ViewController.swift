54cccdcaa5728e972a94bf5531b7866ce379172a
teledi_makhafola <lkgahliso03@gmail.com>
2023-05-22 16:40:49 +0200

    first initial commit

--- TableLessonPractice/ViewController.swift
+++ TableLessonPractice/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var employees = ["Lethabo","Kea","TK","Max","Boipelo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource{
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = employees[indexPath.row]
+        return cell
+    }
+    
+    
+}
