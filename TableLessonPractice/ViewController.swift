29adfa9c92863a6d6a3f9805a85da14e876c4f8b
teledi_makhafola <lkgahliso03@gmail.com>
2023-05-22 16:07:54 +0200

    Initial Commit

---
+++ TableLessonPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableLessonPractice
+//
+//  Created by DA MAC M1 120 on 2023/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
